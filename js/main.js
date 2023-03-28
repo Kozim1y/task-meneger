6a68f252810d1cdcffb53ebb7ea9af584e2cb9c5
Usta_Kozim <87965501+usta-kozim@users.noreply.github.com>
2023-03-28 12:11:11 +0500

    Add calendar and profile edit html

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
-let months = [
+// ========================================
+// ==================Time==================
+// ========================================
+const months = [
 	"yanvar",
 	"fevrel",
 	"mart",
@@ -17,13 +20,13 @@ function load() {
 	let nowHour = document.querySelector(".now-hour-js");
 	let nowMinute = document.querySelector(".now-minute-js");
 	let nowSecond = document.querySelector(".now-second-js");
-	let date = new Date();
+	let newDate = new Date();
 
-	let day = date.getDate();
-	let monthNumber = date.getMonth();
-	let hour = date.getHours();
-	let minute = date.getMinutes();
-	let second = date.getSeconds();
+	let day = newDate.getDate();
+	let monthNumber = newDate.getMonth();
+	let hour = newDate.getHours();
+	let minute = newDate.getMinutes();
+	let second = newDate.getSeconds();
 
 	nowDay.textContent = `${day}-${months[monthNumber]}`;
   nowHour.textContent = `${hour = (hour < 10) ? "0" + hour : hour} :`;
@@ -32,3 +35,58 @@ function load() {
 }
 
 setInterval(load, 999);
+
+
+// ========================================
+// ===========Calendar js==================
+// ========================================
+const currentDate = document.querySelector(".current-date");
+const daysTag = document.querySelector(".days");
+const prevNextIcon = document.querySelectorAll(".prev-next-icon");
+
+
+let date = new Date();
+let currYear = date.getFullYear();
+let currMonth = date.getMonth();
+
+const renderCalendar = () => {
+  let firstDayOfMonth = new Date(currYear, currMonth, 1).getDay();
+  let lastDateOfMonth = new Date(currYear, currMonth + 1, 0).getDate();
+  let lastDayOfMonth = new Date(currYear, currMonth, lastDateOfMonth).getDay();
+  let lastDateOfLastMonth = new Date(currYear, currMonth, 0).getDate();
+  let liTag = "";
+
+	for (let i = firstDayOfMonth; i > 0; i--) {
+    liTag += `<li class="inactive">${lastDateOfLastMonth - i + 1}</li>`		
+	}
+
+  for(let i = 1; i <= lastDateOfMonth; i++) {
+		let isToday = i === date.getDate() && currMonth === new Date().getMonth()
+									&& currYear === new Date().getFullYear() ? "active" : ""
+    liTag += `<li class="${isToday}">${i}</li>`
+  }
+
+	for (let i = lastDayOfMonth; i < 6; i++) {
+		liTag += `<li class="inactive">${i - lastDayOfMonth + 1}</li>`
+	}
+
+  currentDate.innerHTML = `${currYear} ${months[currMonth]} oyi`;
+  daysTag.innerHTML = liTag;
+}
+
+renderCalendar()
+
+prevNextIcon.forEach(icon => {
+  icon.addEventListener("click", () => {
+    currMonth = icon.id ==="prev" ? currMonth - 1 : currMonth + 1;
+
+		if(currMonth < 0 || currMonth > 11) {
+			date = new Date(currYear, currMonth);
+			currYear = date.getFullYear();
+			currMonth = date.getMonth();
+		} else {
+			date = new Date();
+		}
+    renderCalendar()
+  })
+})
